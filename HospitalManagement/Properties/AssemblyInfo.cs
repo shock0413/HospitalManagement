a3277ba1958da4a94865c26462adc5b44baff820
shock0413 <shock0413@gmail.com>
2023-05-22 02:02:41 +0900

    master

--- HospitalManagement/Properties/AssemblyInfo.cs
+++ HospitalManagement/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("KDJ_HospitalManager")]
+[assembly: AssemblyTitle("HospitalManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KDJ_HospitalManager")]
+[assembly: AssemblyProduct("HospitalManagement")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
